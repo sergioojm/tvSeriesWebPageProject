19f7f3b86698109d68c52c9051128fe831807dc7
Vlaskovich <sjulbez@gmail.com>
2025-11-06 12:04:03 +0100

    series card setup

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Search from './components/searchBar/SearchBar'
-
+import Serie from './components/Serie/Serie'
 
 function App() {
   return (
-    <Search placeholder="Buscar serie..." />
+    <>
+      <Search placeholder="Buscar serie..." />
+      <Serie/>
+    </>
+   
   )
 }
 
---
+++ project/src/components/Serie/Serie.jsx
@@ -0,0 +1,23 @@
+import './styleSerie.css';
+import { useState } from 'react';
+
+
+function Serie()
+{
+
+
+    return (
+        
+        <div className='serie-card '>
+            <a class="titulitis" href="https://www.tiktok.com/@fernandoalonso?lang=es" target="_blank">REDES SOCIALES</a>
+            <p class="titulo">EL NANO EN TIKTOK</p>
+            <p class="desc">Fernando Alonso es candidato a ser votado como figura de TikTok de este año.</p>
+        </div>
+    
+    )
+
+}
+
+
+
+export default Serie
